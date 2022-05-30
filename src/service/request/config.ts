cd21fcf0d139b1a607ee87ceeda7538709b40e47
karenL <1739920004@qq.com>
2022-05-30 22:02:14 +0800

    fix: 修复

--- src/service/request/config.ts
+++ src/service/request/config.ts
@@ -1,12 +1,12 @@
-let BASE_URL = ''
+const BASE_URL = '/api'
 const BASE_TIME = 3000
 
-if (process.env.NODE_ENV === 'development') {
-  BASE_URL = '/api'
-} else if (process.env.NODE_ENV === 'production') {
-  BASE_URL = 'http://coderwhy.org/prod'
-} else {
-  BASE_URL = 'http://coderwhy.org/test'
-}
+// if (process.env.NODE_ENV === 'development') {
+//   BASE_URL = '/api'
+// } else if (process.env.NODE_ENV === 'production') {
+//   BASE_URL = 'http://coderwhy.org/prod'
+// } else {
+//   BASE_URL = 'http://coderwhy.org/test'
+// }
 
 export { BASE_URL, BASE_TIME }
